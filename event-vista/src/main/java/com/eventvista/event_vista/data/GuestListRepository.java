d713a43eafd1ee8a5a13384836e5cb0fcc16b3ca
mayurimoreofficial19 <mayurimoreofficial19@gmail.com>
2025-04-18 18:17:18 -0500

    guest-repository updated

--- event-vista/src/main/java/com/eventvista/event_vista/data/GuestListRepository.java
+++ event-vista/src/main/java/com/eventvista/event_vista/data/GuestListRepository.java
@@ -1,11 +1,15 @@
 package com.eventvista.event_vista.data;
 
-import com.eventvista.event_vista.model.GuestList;
+import com.eventvista.event_vista.model.Client;
+import com.eventvista.event_vista.model.Guest;
+import com.eventvista.event_vista.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 
-@Repository
-public interface GuestListRepository extends JpaRepository<GuestList, Integer> {
+public interface GuestRepository extends JpaRepository<Guest, Integer> {
+    Optional<Guest> findByEmailAddress(String emailAddress);
 
+    List<Guest> findByGuestListId(Integer guestListId);
 }
